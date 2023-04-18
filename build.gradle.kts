6b78bd88b73686b3b92deac37010844dfff145ea
Joost van der Griendt <joostvdg@gmail.com>
2023-04-18 10:52:38 +0200

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.0.5"
+	id("io.spring.dependency-management") version "1.1.0"
+	id("org.graalvm.buildtools.native") version "0.9.20"
+}
+
+group = "net.joostvdg.demo.spring.boot"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_HIGHER
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-graphql")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework:spring-webflux")
+	testImplementation("org.springframework.graphql:spring-graphql-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/net/joostvdg/demo/spring/boot/graphql/GraphQlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.joostvdg.demo.spring.boot.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
